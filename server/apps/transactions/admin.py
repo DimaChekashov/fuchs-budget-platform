79043a4d2d3d5869e15f92435fc1c406d6571f16
DimaChekashov <foxdima99@gmail.com>
2026-06-07 09:39:06 +0300

    Remove old code

--- server/apps/transactions/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Transaction
-
-@admin.register(Transaction)
-class TransactionAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'value', 'created_at']
-    list_filter = ['created_at']
-    search_fields = ['title']
